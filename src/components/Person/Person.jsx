7ef045bf9ae6d29bdcec54a98d7f9f716fa7b674
Jakub Bajkowski <k.bajkowski7@wp.pl>
2024-09-23 15:44:16 +0200

    add task solution

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,17 +1,19 @@
 export const Person = ({ person }) => {
-  const partnerSex = person.sex === 'm' ? 'wife' : 'husband';
+  const { name, age, isMarried, partnerName, sex } = person;
+
+  const partnerSex = sex === 'm' ? 'wife' : 'husband';
+
+  const marriageStatus = isMarried
+    ? `${partnerName} is my ${partnerSex}`
+    : 'I am not married';
 
   return (
     <section className="Person">
-      <h2 className="Person__name">My name is {person.name}</h2>
+      <h2 className="Person__name">My name is {name}</h2>
 
-      {person.age && <p className="Person__age">I am {person.age}</p>}
+      {age && <p className="Person__age">I am {age}</p>}
 
-      <p className="Person__partner">
-        {person.isMarried
-          ? `${person.partnerName} is my ${partnerSex}`
-          : 'I am not married'}
-      </p>
+      <p className="Person__partner">{marriageStatus}</p>
     </section>
   );
 };
